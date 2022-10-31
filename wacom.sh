3494d251fabdef58ca46135c4c7aa49357a30b3e
vmr48-ua <vmr48@alu.ua.es>
2022-10-31 16:29:38 +0100

    prueba

---
+++ wacom.sh
@@ -0,0 +1,4 @@
+xsetwacom set "Wacom One by Wacom M Pen stylus" Button 3 key "lalt" "." "-lalt" "u" "p"
+xsetwacom set "Wacom One by Wacom M Pen stylus" Button 2 key "lalt" "." "-lalt" "u" "e"
+xsetwacom set "Wacom One by Wacom M Pen stylus" Button 1 1
+xsetwacom set "Wacom One by Wacom M Pen stylus" Rotate none
---
+++ xournal.sh
@@ -0,0 +1,4 @@
+xsetwacom set "Wacom One by Wacom M Pen stylus" Button 3 key "shift" "control" "p"
+xsetwacom set "Wacom One by Wacom M Pen stylus" Button 2 key "shift" "control" "e"
+xsetwacom set "Wacom One by Wacom M Pen stylus" Button 1 1
+xsetwacom set "Wacom One by Wacom M Pen stylus" Rotate none
