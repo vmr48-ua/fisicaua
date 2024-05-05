ff25da3d44a12d92e8e31a88cafd5b3331fc1617
vmr48-ua <vmr48@gcloud.ua.es>
2024-05-05 12:25:02 -0500

    Post notas

--- tercero/SIUE/photonics/Experiment #6_ Preparing and inspecting optical fibers/PHOT-E6.py
+++ tercero/SIUE/photonics/Experiment #6_ Preparing and inspecting optical fibers/PHOT-E6.py
@@ -8,15 +8,7 @@ import atexit
 
 @atexit.register
 def exit_guard() -> None:
-    ...
-
-def plot(x,y,title='',figure=True,figsize=(10,8),grid=False) -> None:
-    if figure:
-        plt.figure(figsize)
-    if grid:
-        plt.grid()
-    plt.title(title)
-    plt.plot(x,y)
+    plt.show()
 
 def main() -> None:
     data = np.array([1.5,2.5,4,5,6.5])
@@ -25,8 +17,10 @@ def main() -> None:
     x = [1,2,3,4,5]
 
     plt.scatter(x,y,marker='x')
-    plt.axhline(np.average(y),)
-    plt.show()
+    plt.axhline(np.average(y))
+    plt.xlabel('m')
+    plt.ylabel('D(m)')
+    print(np.average(y))
 
 if __name__ == '__main__':
     main()
